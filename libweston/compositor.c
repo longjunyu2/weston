longjunyu2/weston

commit 0f99e081c43a0f60cfc03463e6a491424a2bb7c4

surface: Add input-region dirty flag
Used when the input region changes.

Signed-off-by: Daniel Stone <daniels@collabora.com>

libweston/compositor.c
@@ -3880,6 +3880,8 @@ surface_set_input_region(struct wl_client *client,
 		pixman_region32_fini(&surface->pending.input);
 		region_init_infinite(&surface->pending.input);
 	}
+
+	surface->pending.status |= WESTON_SURFACE_DIRTY_INPUT;
 }
 
 /* Cause damage to this sub-surface and all its children.
@@ -4199,8 +4201,11 @@ weston_surface_commit_state(struct weston_surface *surface,
 	}
 
 	/* wl_surface.set_input_region */
-	pixman_region32_intersect_rect(&surface->input, &state->input,
-				       0, 0, surface->width, surface->height);
+	if (status & (WESTON_SURFACE_DIRTY_SIZE | WESTON_SURFACE_DIRTY_INPUT)) {
+		pixman_region32_intersect_rect(&surface->input, &state->input,
+				       	       0, 0,
+					       surface->width, surface->height);
+	}
 
 	/* wl_surface.frame */
 	wl_list_insert_list(&surface->frame_callback_list,

include/libweston/libweston.h
@@ -1740,6 +1740,8 @@ enum weston_surface_status {
 	/** buffer parameters have changed in a way which impacts the
 	 * processing pipeline, e.g. format/opacity */
 	WESTON_SURFACE_DIRTY_BUFFER_PARAMS = 1 << 3,
+	/** input region has changed */
+	WESTON_SURFACE_DIRTY_INPUT = 1 << 4,
 };
 
 struct weston_surface_state {

commit d500dbd4e2c97c83f82ee36739f70cce713ee9bc

xwm: Fix input regions
commit 0f99e081c43a0f60cfc03463e6a491424a2bb7c4 broke xwayland client
input regions. The xwayland window manager sets input regions in the
pending state without going through the function that sets the
WESTON_SURFACE_DIRTY_INPUT bit.

Just add the dirty bit to the xwm code.

Also, fix the whitespace error the same patch introduced.

fixes 0f99e081c43a0f60cfc03463e6a491424a2bb7c4

Signed-off-by: Derek Foreman <derek.foreman@collabora.com>

libweston/compositor.c
@@ -4801,7 +4801,7 @@ weston_surface_commit_state(struct weston_surface *surface,
 	/* wl_surface.set_input_region */
 	if (status & (WESTON_SURFACE_DIRTY_SIZE | WESTON_SURFACE_DIRTY_INPUT)) {
 		pixman_region32_intersect_rect(&surface->input, &state->input,
-				       	       0, 0,
+					       0, 0,
 					       surface->width, surface->height);
 	}
 

xwayland/window-manager.c
@@ -1442,6 +1442,7 @@ weston_wm_window_set_pending_state(struct weston_wm_window *window)
 	pixman_region32_fini(&window->surface->pending.input);
 	pixman_region32_init_rect(&window->surface->pending.input,
 				  input_x, input_y, input_w, input_h);
+	window->surface->pending.status |= WESTON_SURFACE_DIRTY_INPUT;
 
 	xwayland_interface->set_window_geometry(window->shsurf,
 						input_x, input_y,